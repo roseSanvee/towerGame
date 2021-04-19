f9d5d4ced60bc5eda77a7313713f45c14d85d290
roseSanvee <75472083+roseSanvee@users.noreply.github.com>
2021-04-19 15:05:29 +0530

    Add files via upload

---
+++ Tower-Siege-1-Template-main/slingshot.js
@@ -0,0 +1,15 @@
+//class SlingShot()
+  //  constructor(bodyA, bodyB){
+    //    var options = {
+      //      bodyA: bodyA,
+        ////    bodyB: bodyB,
+            //stiffness: 0.04,
+ //           length: 10
+   //     }
+   // }
+//
+  //  fly(){
+    //    this.sling.bodyA = null;
+    //}
+
+    
